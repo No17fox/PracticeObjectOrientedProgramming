eee3ae62bfa531e63c39f7cf98dfb8c1f894785e
WangLei <wanglei0717@outlook.com>
2019-02-18 16:55:30 +0800

    Add a class named 'Person'

--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -1,8 +1,18 @@
 package main.java.company;
 
+import main.java.company.model.Person;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Person person1 = new Person(0001, "Tom", 20);
+        Person person2 = new Person(0001, "Tom", 20);
+        Person person3 = new Person(0002, "Tom", 20);
+        System.out.println(person1.introduce());
+        System.out.println(person1.equals(person2));
+        System.out.println(person1.equals(person3));
+        System.out.println(person1.hashCode() == person2.hashCode());
+        System.out.println(person1.hashCode() == person3.hashCode());
+
     }
 }
---
+++ src/main/java/company/model/Person.java
@@ -0,0 +1,39 @@
+package main.java.company.model;
+
+public class Person {
+    private final int id;
+    private final String name;
+    private final int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return this.id == ((Person)object).getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id;
+    }
+}
